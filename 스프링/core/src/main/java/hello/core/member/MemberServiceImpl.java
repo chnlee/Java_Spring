3dadaa48df33cac0e6b1d4aa98d1a3679e5e432f
chnlee <84317436+chnlee@users.noreply.github.com>
2022-04-11 23:54:21 +0900

    Add files via upload

--- 스프링/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 스프링/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,8 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
     // final은 한 번 값을 설정하면 그 값을 다시 설정할 수 없다는 뜻이다.
-    // MemberRepoistory(추상화), MemoryMemberRepository(구체화)가 동시에 의존되었기 때문에 DIP 위반이다.
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // MemberRepository(추상화), MemoryMemberRepository(구체화)가 동시에 의존되었기 때문에 DIP 위반이다.
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
